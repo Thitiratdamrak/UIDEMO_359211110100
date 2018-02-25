0b414b635c5b1791cd17be3dca6b146a1051b44e
Thitiratdamrak <Fonhmi0223@gmail.com>
2018-02-25 22:25:38 +0700

    adminController

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,8 +18,11 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
-    private loginModel loginModel = new loginModel();
+public class Controller implements Initializable {
+    private loginModel loginmodel = new loginModel();
+
+    @FXML
+    private Label loginStatus;
 
     @FXML
     private JFXTextField username;
@@ -31,44 +34,42 @@ public class Controller implements Initializable{
     private JFXButton btnLogin;
 
     @FXML
-    private Label loginStatus;
-
-    @FXML
     private Label dbStatus;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        if (this.loginModel.isDatabaseConnection()) {
+        if (this.loginmodel.isDatabaseConnection()) {
             this.dbStatus.setText("Connected to DB.");
-        }else {
+        }else{
             this.dbStatus.setText("Not Connect to DB.");
         }
+
     }//initialize
 
     @FXML
-    public void Login(ActionEvent event) {
+    public  void Login(ActionEvent event){
         try {
-            if (this.loginModel.isLogin(username.getText(),password.getText())){
+            if (this.loginmodel.isLogin(username.getText(),password.getText())) {
                 Stage stage = (Stage) this.btnLogin.getScene().getWindow();
                 stage.close();
-                adminDashboard();
-
-            }else {
-                loginStatus.setText("Your username or password is invalid");
+                adminDashborad();
+            } else {
+                loginStatus.setText ("Your username or password is invalid");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }//Login
 
-    private void adminDashboard() {
+
+    }//login
+
+    private void adminDashborad() {
         try {
             Stage adminStage = new Stage();
             FXMLLoader adminLoader = new FXMLLoader();
-            Pane adminRoot = (Pane) adminLoader.load(
-                    getClass().getResource("/admin/adminDashboard.fxml").openStream());
+            Pane adminRoot = (Pane) adminLoader.load(getClass().getResource("/admin/adminDashboard.fxml").openStream());
             adminController adminController = adminLoader.getController();
             Scene scene = new Scene(adminRoot);
             adminStage.setScene(scene);
@@ -79,7 +80,7 @@ public class Controller implements Initializable{
             e.printStackTrace();
         }
 
-
     }//adminDashboard
+
 
 }//class
